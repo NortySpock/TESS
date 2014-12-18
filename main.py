7bef3f7af567c770a9e83c30b581150e4337c640
David Norton <david.norton@cerner.com>
2014-12-17 20:45:20 -0600

    stablized overbooking protection code

--- main.py
+++ main.py
@@ -18,12 +18,12 @@ number_of_meetings = 3
 max_role_name_length = 0
 max_person_name_length = 0
 
-
+#files we're reading and writing
 tm_file_name = "toastmasters.txt"
 role_file_name = "roles.txt"
 suggested_roles_file_name = "suggested_roles.txt"
 
-
+#read TM file
 tm_file = open(tm_file_name, 'r')
 for line in tm_file:
     tmp_line = line.strip()
@@ -33,6 +33,7 @@ for line in tm_file:
             max_person_name_length = len(tmp_line)
 tm_file.close()
 
+#read roles file
 role_file = open(role_file_name,'r')
 for line in role_file:
     tmp_line = line.strip()
@@ -51,7 +52,6 @@ else:
 
 for meeting in range(1,number_of_meetings+1):
     write_lines.append("Meeting #"+str(meeting)+linesep)
-    #format_line = '{:'+str(max_role_name_length)+'s}'
     meeting_people = []
 
     for role in list_of_roles:
@@ -63,16 +63,21 @@ for meeting in range(1,number_of_meetings+1):
         #we've already assigned a member to a previous role and we care about that
 
         while(proposed_member in meeting_people and prevent_overbooking == True):
-            safety += 1
+            # If we run into a case where a person was booked for two roles during a meeting
+            # dump the list of people on the back of the list (to prevent infinite loops of people)
+            # and bump them to the back of the newly padded list so they get recycled.
+            # It's not really pretty, but it gets the job done.
             shuffle(list_of_people)
-            available_people.extend(list_of_people) #this isn't a really clean way to do it, but
+            available_people.extend(list_of_people)
             available_people.insert(0,proposed_member) #move member to back of list
             proposed_member = available_people.pop()
-            if(safety > 1000):
 
+        # assuming we have a non-duplicate member, append them to the meeting list
         meeting_people.append(proposed_member)
 
+    # match people with roles
     suggested_meeting_roles = zip(list_of_roles,meeting_people)
+    # do all the ugly formatting and add it to the outgoing list
     for smr in suggested_meeting_roles:
         write_lines.append(smr[0].strip()+(" "*(max_role_name_length+2-len(smr[0])))+smr[1].strip()+linesep)
     write_lines.append(linesep)
