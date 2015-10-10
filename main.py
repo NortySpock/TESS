ea25d469e0e0e82dd383e963f43e59265b3bbaad
David Norton <david.norton@cerner.com>
2015-10-10 07:04:41 -0500

    Added more comments as well as CSV output support.

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+# TESS - ToastmastErs Simple Scheduler
+# This simple script will create a shuffled list of suggested meeting roles for multiple meetings. 
+
 from random import shuffle
 from os import linesep
+import csv
 
-# full list of people and roles
+# full list of people and roles, 
 list_of_people = []
 list_of_roles = []
 
@@ -9,10 +13,13 @@ list_of_roles = []
 available_people = []
 meeting_people = []
 
-# file to be written
+# a list of completed meetings
+multi_meeting_list = []
+
+#cheap text holding list
 write_lines = []
 
-number_of_meetings = 3
+number_of_meetings = 14
 
 #keeps track of width of people so we can pad apppropriately
 max_role_name_length = 0
@@ -21,7 +28,9 @@ max_person_name_length = 0
 #files we're reading and writing
 tm_file_name = "toastmasters.txt"
 role_file_name = "roles.txt"
-suggested_roles_file_name = "suggested_roles.txt"
+suggested_roles_text_file_name = "suggested_roles.txt"
+suggested_roles_csv_file_name = "suggested_roles.csv"
+
 
 #read TM file
 tm_file = open(tm_file_name, 'r')
@@ -43,25 +52,28 @@ for line in role_file:
             max_role_name_length = len(tmp_line)
 role_file.close()
 
-# if we don't have enough people we can't prevent overbooking
+# if we have enough people per meeting we can prevent overbooking
+prevent_overbooking = False
 if(len(list_of_roles) < len(list_of_people)):
-    prevent_overbooking = False
-else:
     prevent_overbooking = True
-
-
+    
+#Main segment where each meeting is populated
 for meeting in range(1,number_of_meetings+1):
-    write_lines.append("Meeting #"+str(meeting)+linesep)
     meeting_people = []
 
+    # Each meeting has a number of roles. (Toastmaster, Speaker 1, Evaluator 1, etc)
+    # We will shuffle the person list, select an available person and, if they have not been added to the meeting yet, add them.
     for role in list_of_roles:
-        #if we're out of roles
+        #if we're out of people for roles, refill the buffer
         if(len(available_people)==0):
             shuffle(list_of_people)
             available_people.extend(list_of_people)
+        
+        #select a person
         proposed_member = available_people.pop()
-        #we've already assigned a member to a previous role and we care about that
-
+        
+        
+        #If we've already assigned a member to a previous role and we can prevent double-booking
         while(proposed_member in meeting_people and prevent_overbooking == True):
             # If we run into a case where a person was booked for two roles during a meeting
             # dump the list of people on the back of the list (to prevent infinite loops of people)
@@ -72,19 +84,36 @@ for meeting in range(1,number_of_meetings+1):
             available_people.insert(0,proposed_member) #move member to back of list
             proposed_member = available_people.pop()
 
-        # assuming we have a non-duplicate member, append them to the meeting list
+
+        # assuming we have a non-duplicate member (if we prevent overbooking), so append them to the meeting list
         meeting_people.append(proposed_member)
 
-    # match people with roles
+
+    # Now that we have a list of people, match people with roles
     suggested_meeting_roles = zip(list_of_roles,meeting_people)
-    # do all the ugly formatting and add it to the outgoing list
-    for smr in suggested_meeting_roles:
-        write_lines.append(smr[0].strip()+(" "*(max_role_name_length+2-len(smr[0])))+smr[1].strip()+linesep)
+    multi_meeting_list.append(suggested_meeting_roles)
+
+write_lines = []
+for i, mtg in enumerate(multi_meeting_list):    
+    # do all the ugly text formatting and add it to the outgoing text
+    write_lines.append("Meeting #"+str(i+1)+linesep)
+
+    for role in mtg:
+        write_lines.append(role[0].strip()+(" "*(max_role_name_length+2-len(role[0])))+role[1].strip()+linesep)
     write_lines.append(linesep)
+    
 
+with open(suggested_roles_text_file_name, 'w') as f:
+    for line in write_lines:
+          f.write(line)
 
-sug_role_file = open(suggested_roles_file_name,'w')
-for line in write_lines:
-    sug_role_file.write(line)
-sug_role_file.close()
+csv_rows = []
+for i, mtg in enumerate(multi_meeting_list):
+    csv_rows.append(("Meeting #"+str(i+1),))
+    for role in mtg:
+        csv_rows.append((role[0].strip(),role[1].strip()))
+    csv_rows.append("")
 
+with open(suggested_roles_csv_file_name, 'wb') as f:
+    writer = csv.writer(f,dialect='excel')
+    writer.writerows(csv_rows)
